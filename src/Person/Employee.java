89dac713a4e87bf331d6e7b72b5d7b7e60b852a9
Nova <110995638+NovaCaliber@users.noreply.github.com>
2024-12-04 03:49:50 +0000

    Updated the Employee Class

--- src/Person/Employee.java
+++ src/Person/Employee.java
@@ -1,4 +1,14 @@
 package Person;
 
-public class Employee {
+public class Employee extends Person {
+    private int empNo;
+
+    public Employee(int empNo, String firstName, String lastName, String email, String address) {
+        super(firstName, lastName, email, address);
+        this.empNo = empNo;
+    }
+
+    public int getEmpNo() {
+        return empNo;
+    }
 }
