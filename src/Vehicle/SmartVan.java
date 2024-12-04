005715176901ed104f2a3ef3fcc72e6a2626e306
Nova <110995638+NovaCaliber@users.noreply.github.com>
2024-12-04 03:58:33 +0000

    Updated the SmartVan Class

--- src/Vehicle/SmartVan.java
+++ src/Vehicle/SmartVan.java
@@ -1,4 +1,42 @@
 package Vehicle;
 
-public class SmartVan {
+import java.time.LocalDate;
+
+public class SmartVan extends Vehicle {
+    private double engine;
+    private int co2;
+
+    public SmartVan(int id, String regNo, String manufacturer, String model,
+                     LocalDate regDate, String location,
+                     double engine, int co2) {
+        super(id, regNo, manufacturer, model, regDate, location);
+        this.engine = engine;
+        this.co2 = co2;
+    }
+
+    public double getEngine() {
+        return engine;
+    }
+
+    public void setEngine(double engine) {
+        this.engine = engine;
+    }
+
+    public int getCo2() {
+        return co2;
+    }
+
+    public void setCo2(int co2) {
+        this.co2 = co2;
+    }
+
+
+
+    @Override
+    public double calculateRate(int hours, double distance) {
+        double hourlyRate = 11.0;
+        double mileageRate = (distance > 50) ? (distance - 50) * 0.25 : 0;
+        return hours * hourlyRate + mileageRate;
+    }
+
 }
